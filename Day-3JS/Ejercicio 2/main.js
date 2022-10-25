2a4c75645b91a1a94edd58f8ab364293eb3e4f45
nelchacin <nelchacindev@gmail.com>
2022-10-25 14:15:29 +0200

    last commit

---
+++ Day-3JS/Ejercicio 2/main.js
@@ -0,0 +1,57 @@
+// EJERCICIO 1
+
+
+// let numero= prompt ("Ingrese un número")
+
+// for (let i=1; i<=numero; i++){
+//   document.write(`${i}<br>`)
+// }
+
+// EJERCICIO 2
+
+// let numero= prompt ("Ingrese un número")
+
+// for (let i=1; i<=numero; i=i+2){
+//   document.write(`${i}<br>`)
+// }
+
+// EJERCICIO 3
+
+// let numero= prompt ("Ingrese un número")
+
+// for (let i=numero; i>=1; i--){
+//   document.write(`${i}<br>`)
+// }
+
+// EJERCICIO 4
+
+// for (let i=0; i<=90; i= i+9){
+//   console.log (i)
+//  }
+
+EJERCICIO 6
+
+let numero1 = prompt ("Introduce un número")
+ let suma1 =0
+ let numero2 = prompt ("Introduce otro número")
+ let suma2 =0
+ 
+ for (let i=1; i<=numero1; i++){
+    if (i%2==0){
+    (suma1=suma1+i)
+    } 
+}
+for (let i=1; i<=numero2; i++){
+    if (i%2==0){
+    (suma2=suma2+i)
+    } 
+}
+alert (suma1+suma2)
+
+
+
+
+
+
+
+
